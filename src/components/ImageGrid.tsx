a8ebf09a5e29e08e9236d9a7e3856cc8ad9e3c21
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-04-18 22:31:06 +0200

    feat(ImageGrid):Add setCurrent image prop and current index prop

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { ImageGridProps } from '../types';
 import ImageCard from './ImageCard';
-const ImageGrid: React.FC<ImageGridProps> = ({ images }) => {
+const ImageGrid: React.FC<ImageGridProps> = ({ images, setCurrent }) => {
   return (
     <div id='image-grid' className='container grid grid-cols-4 mx-auto gap-2'>
-      {images?.map((image) => (
-        <ImageCard image={image} key={image.uuid} />
+      {images?.map((image, index) => (
+        <ImageCard
+          image={image}
+          key={image.uuid}
+          setCurrent={setCurrent}
+          index={index}
+        />
       ))}
     </div>
   );
