315977ab32429623095553de577db671cc16f944
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-04-18 22:31:17 +0200

    feat(ImageCard):Add setCurrent image prop and current index prop

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { ImageData } from '../types';
-const ImageCard: React.FC<{ image: ImageData }> = ({ image }) => {
+const ImageCard: React.FC<{
+  image: ImageData;
+  setCurrent: React.Dispatch<React.SetStateAction<number>>;
+  index: number;
+}> = ({ image, index, setCurrent }) => {
   return (
-    <div>
+    <div className='group relative'>
       <img src={image.url} alt={image.name} className='aspect-video' />
+      <div
+        onClick={() => {
+          setCurrent(index);
+        }}
+        className='absolute inset-0  hidden group-hover:flex items-center justify-center cursor-pointer select-none'
+      >
+        <div className='bg-black opacity-50 w-full h-full absolute'></div>
+        <span className='text-3xl z-50'>🔍</span>
+      </div>
     </div>
   );
 };
